8d1c986aa58c4e8e821a42f9efb5656e4a8e1e34
Meaghan Zorij <meaghan@epiqsolutions.com>
2019-09-13 09:28:10 -0500

    Minor updates to example 1PPS flow graph

--- examples/top_block.py
+++ examples/top_block.py
@@ -71,13 +71,6 @@ class top_block(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
-        self._sample_rate_0_range = Range(1e6, max_sample_rate, 1e6, 50e6, 200)
-        self._sample_rate_0_win = RangeWidget(self._sample_rate_0_range, self.set_sample_rate_0, 'Sample Rate', "counter_slider", float)
-        self.top_grid_layout.addWidget(self._sample_rate_0_win, 1, 0, 1, 1)
-        for r in range(1, 2):
-            self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(0, 1):
-            self.top_grid_layout.setColumnStretch(c, 1)
         self._center_freq_range = Range(100e6, 6000e6, 1e6, 915e6, 200)
         self._center_freq_win = RangeWidget(self._center_freq_range, self.set_center_freq, 'Frequency', "counter_slider", float)
         self.top_grid_layout.addWidget(self._center_freq_win, 0, 0, 1, 1)
@@ -86,49 +79,20 @@ class top_block(gr.top_block, Qt.QWidget):
         for c in range(0, 1):
             self.top_grid_layout.setColumnStretch(c, 1)
         self._bandwidth_range = Range(1e6, max_sample_rate, 1e6, max_sample_rate, 200)
-        self._bandwidth_win = RangeWidget(self._bandwidth_range, self.set_bandwidth, 'Bandwidth', "slider", float)
-        self.top_grid_layout.addWidget(self._bandwidth_win, 1, 1, 1, 1)
+        self._bandwidth_win = RangeWidget(self._bandwidth_range, self.set_bandwidth, 'Bandwidth', "counter_slider", float)
+        self.top_grid_layout.addWidget(self._bandwidth_win, 2, 0, 1, 1)
+        for r in range(2, 3):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(0, 1):
+            self.top_grid_layout.setColumnStretch(c, 1)
+        self.sidekiq_sidekiq_rx_0_0 = sidekiq.sidekiq_rx(sample_rate, 50, 1, center_freq, bandwidth, 1, vector_length, (),1)
+        self._sample_rate_0_range = Range(1e6, max_sample_rate, 1e6, 50e6, 200)
+        self._sample_rate_0_win = RangeWidget(self._sample_rate_0_range, self.set_sample_rate_0, 'Sample Rate', "counter_slider", float)
+        self.top_grid_layout.addWidget(self._sample_rate_0_win, 1, 0, 1, 1)
         for r in range(1, 2):
             self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(1, 2):
+        for c in range(0, 1):
             self.top_grid_layout.setColumnStretch(c, 1)
-        self.sidekiq_sidekiq_rx_0_0 = sidekiq.sidekiq_rx(sample_rate, 50, 1, center_freq, bandwidth, 1, vector_length, (),0)
-        self.qtgui_waterfall_sink_x_0 = qtgui.waterfall_sink_c(
-        	1024, #size
-        	firdes.WIN_BLACKMAN_hARRIS, #wintype
-        	0, #fc
-        	sample_rate_0, #bw
-        	"", #name
-                1 #number of inputs
-        )
-        self.qtgui_waterfall_sink_x_0.set_update_time(0.10)
-        self.qtgui_waterfall_sink_x_0.enable_grid(False)
-        self.qtgui_waterfall_sink_x_0.enable_axis_labels(True)
-
-        if not True:
-          self.qtgui_waterfall_sink_x_0.disable_legend()
-
-        if "complex" == "float" or "complex" == "msg_float":
-          self.qtgui_waterfall_sink_x_0.set_plot_pos_half(not True)
-
-        labels = ['', '', '', '', '',
-                  '', '', '', '', '']
-        colors = [0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-                  1.0, 1.0, 1.0, 1.0, 1.0]
-        for i in xrange(1):
-            if len(labels[i]) == 0:
-                self.qtgui_waterfall_sink_x_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_waterfall_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_waterfall_sink_x_0.set_color_map(i, colors[i])
-            self.qtgui_waterfall_sink_x_0.set_line_alpha(i, alphas[i])
-
-        self.qtgui_waterfall_sink_x_0.set_intensity_range(-140, 10)
-
-        self._qtgui_waterfall_sink_x_0_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_waterfall_sink_x_0_win)
         self.qtgui_time_sink_x_1 = qtgui.time_sink_c(
         	2**14, #size
         	sample_rate, #samp_rate
@@ -145,10 +109,10 @@ class top_block(gr.top_block, Qt.QWidget):
         self.qtgui_time_sink_x_1.enable_autoscale(False)
         self.qtgui_time_sink_x_1.enable_grid(False)
         self.qtgui_time_sink_x_1.enable_axis_labels(True)
-        self.qtgui_time_sink_x_1.enable_control_panel(False)
+        self.qtgui_time_sink_x_1.enable_control_panel(True)
         self.qtgui_time_sink_x_1.enable_stem_plot(False)
 
-        if not True:
+        if not False:
           self.qtgui_time_sink_x_1.disable_legend()
 
         labels = ['', '', '', '', '',
@@ -191,14 +155,14 @@ class top_block(gr.top_block, Qt.QWidget):
         self.qtgui_freq_sink_x_0.set_update_time(0.01)
         self.qtgui_freq_sink_x_0.set_y_axis(-140, 10)
         self.qtgui_freq_sink_x_0.set_y_label('Relative Gain', 'dB')
-        self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
+        self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "rx_freq")
         self.qtgui_freq_sink_x_0.enable_autoscale(False)
         self.qtgui_freq_sink_x_0.enable_grid(False)
         self.qtgui_freq_sink_x_0.set_fft_average(0.1)
         self.qtgui_freq_sink_x_0.enable_axis_labels(True)
         self.qtgui_freq_sink_x_0.enable_control_panel(False)
 
-        if not True:
+        if not False:
           self.qtgui_freq_sink_x_0.disable_legend()
 
         if "complex" == "float" or "complex" == "msg_float":
@@ -232,7 +196,6 @@ class top_block(gr.top_block, Qt.QWidget):
         ##################################################
         self.connect((self.blocks_conjugate_cc_0, 0), (self.qtgui_freq_sink_x_0, 0))
         self.connect((self.blocks_conjugate_cc_0, 0), (self.qtgui_time_sink_x_1, 0))
-        self.connect((self.blocks_conjugate_cc_0, 0), (self.qtgui_waterfall_sink_x_0, 0))
         self.connect((self.sidekiq_sidekiq_rx_0_0, 0), (self.blocks_conjugate_cc_0, 0))
 
     def closeEvent(self, event):
@@ -258,7 +221,6 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_sample_rate_0(self, sample_rate_0):
         self.sample_rate_0 = sample_rate_0
-        self.qtgui_waterfall_sink_x_0.set_frequency_range(0, self.sample_rate_0)
 
     def get_sample_rate(self):
         return self.sample_rate
