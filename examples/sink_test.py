1bc67ce46f8ce89085b071b4027581f750512c31
dave helm <dave.helm@epiqsolutions.com>
2025-11-17 14:39:00 -0600

    quad_source example app

--- examples/sink_test.py
+++ examples/sink_test.py
@@ -6,37 +6,25 @@
 #
 # GNU Radio Python Flow Graph
 # Title: Sink Test
-# GNU Radio version: v3.11.0.0git-375-ge2af6089
-
-from packaging.version import Version as StrictVersion
-
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print("Warning: failed to XInitThreads()")
+# GNU Radio version: v3.11.0.0git-605-g9b22fd38
 
 from PyQt5 import Qt
+from gnuradio import qtgui
 from gnuradio import analog
 from gnuradio import gr
 from gnuradio.filter import firdes
 from gnuradio.fft import window
 import sys
 import signal
+from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
-from gnuradio import sidekiq
+from gnuradio import soapy
 from gnuradio.qtgui import Range, RangeWidget
 from PyQt5 import QtCore
 
 
-
-from gnuradio import qtgui
 
 class sink_test(gr.top_block, Qt.QWidget):
 
@@ -47,8 +35,8 @@ class sink_test(gr.top_block, Qt.QWidget):
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-            pass
+        except BaseException as exc:
+            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
         self.top_scroll_layout = Qt.QVBoxLayout()
         self.setLayout(self.top_scroll_layout)
         self.top_scroll = Qt.QScrollArea()
@@ -64,12 +52,11 @@ class sink_test(gr.top_block, Qt.QWidget):
         self.settings = Qt.QSettings("GNU Radio", "sink_test")
 
         try:
-            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-                self.restoreGeometry(self.settings.value("geometry").toByteArray())
-            else:
-                self.restoreGeometry(self.settings.value("geometry"))
-        except:
-            pass
+            geometry = self.settings.value("geometry")
+            if geometry:
+                self.restoreGeometry(geometry)
+        except BaseException as exc:
+            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
 
         ##################################################
         # Variables
@@ -77,7 +64,7 @@ class sink_test(gr.top_block, Qt.QWidget):
         self.sample_rate = sample_rate = 20e6
         self.tone_freq = tone_freq = 2e6
         self.run_tx_calibration = run_tx_calibration = 0
-        self.min_output_buffer = min_output_buffer = 32764 *2*2
+        self.min_buffer = min_buffer = 16380
         self.frequency = frequency = 1000e6
         self.bandwidth = bandwidth = sample_rate * 0.8
         self.attenuation = attenuation = 150
@@ -86,13 +73,6 @@ class sink_test(gr.top_block, Qt.QWidget):
         # Blocks
         ##################################################
 
-        self._sample_rate_range = Range(1e6, 250e6, 1e6, 20e6, 200)
-        self._sample_rate_win = RangeWidget(self._sample_rate_range, self.set_sample_rate, "'sample_rate'", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_grid_layout.addWidget(self._sample_rate_win, 0, 0, 2, 1)
-        for r in range(0, 2):
-            self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(0, 1):
-            self.top_grid_layout.setColumnStretch(c, 1)
         self._tone_freq_range = Range(1e6, 25e6, 1e6, 2e6, 200)
         self._tone_freq_win = RangeWidget(self._tone_freq_range, self.set_tone_freq, "'tone_freq'", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_grid_layout.addWidget(self._tone_freq_win, 10, 0, 2, 1)
@@ -100,10 +80,43 @@ class sink_test(gr.top_block, Qt.QWidget):
             self.top_grid_layout.setRowStretch(r, 1)
         for c in range(0, 1):
             self.top_grid_layout.setColumnStretch(c, 1)
+        self._sample_rate_range = Range(1e6, 250e6, 1e6, 20e6, 200)
+        self._sample_rate_win = RangeWidget(self._sample_rate_range, self.set_sample_rate, "'sample_rate'", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_grid_layout.addWidget(self._sample_rate_win, 0, 0, 2, 1)
+        for r in range(0, 2):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(0, 1):
+            self.top_grid_layout.setColumnStretch(c, 1)
         self._frequency_range = Range(500e6, 6000e6, 1e6, 1000e6, 100)
         self._frequency_win = RangeWidget(self._frequency_range, self.set_frequency, "Frequency", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_grid_layout.addWidget(self._frequency_win, 4, 0, 2, 1)
         for r in range(4, 6):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(0, 1):
+            self.top_grid_layout.setColumnStretch(c, 1)
+        self.soapy_custom_sink_0 = None
+        dev = 'driver=' + 'sidekiq'
+        stream_args = ''
+        tune_args = ['']
+        settings = ['']
+        self.soapy_custom_sink_0 = soapy.sink(dev, "fc32",
+                                1, 'card=2',
+                                stream_args, tune_args, settings)
+        self.soapy_custom_sink_0.set_sample_rate(0, sample_rate)
+        self.soapy_custom_sink_0.set_bandwidth(0, 0)
+        self.soapy_custom_sink_0.set_antenna(0, 'TX')
+        self.soapy_custom_sink_0.set_frequency(0, frequency)
+        self.soapy_custom_sink_0.set_frequency_correction(0, 0)
+        self.soapy_custom_sink_0.set_gain(0, 60)
+        self.soapy_custom_sink_0.set_dc_offset(0, 0)
+        self.soapy_custom_sink_0.set_iq_balance(0, 0)
+        _run_tx_calibration_push_button = Qt.QPushButton('')
+        _run_tx_calibration_push_button = Qt.QPushButton('run_tx_calibration')
+        self._run_tx_calibration_choices = {'Pressed': 1, 'Released': 0}
+        _run_tx_calibration_push_button.pressed.connect(lambda: self.set_run_tx_calibration(self._run_tx_calibration_choices['Pressed']))
+        _run_tx_calibration_push_button.released.connect(lambda: self.set_run_tx_calibration(self._run_tx_calibration_choices['Released']))
+        self.top_grid_layout.addWidget(_run_tx_calibration_push_button, 12, 0, 1, 1)
+        for r in range(12, 13):
             self.top_grid_layout.setRowStretch(r, 1)
         for c in range(0, 1):
             self.top_grid_layout.setColumnStretch(c, 1)
@@ -121,25 +134,13 @@ class sink_test(gr.top_block, Qt.QWidget):
             self.top_grid_layout.setRowStretch(r, 1)
         for c in range(0, 1):
             self.top_grid_layout.setColumnStretch(c, 1)
-        self.sidekiq_sidekiq_tx_0 = sidekiq.sidekiq_tx(1, 0, sample_rate, bandwidth, frequency, attenuation, '', 4, 32764, 1)
-        _run_tx_calibration_push_button = Qt.QPushButton('')
-        _run_tx_calibration_push_button = Qt.QPushButton('run_tx_calibration')
-        self._run_tx_calibration_choices = {'Pressed': 1, 'Released': 0}
-        _run_tx_calibration_push_button.pressed.connect(lambda: self.set_run_tx_calibration(self._run_tx_calibration_choices['Pressed']))
-        _run_tx_calibration_push_button.released.connect(lambda: self.set_run_tx_calibration(self._run_tx_calibration_choices['Released']))
-        self.top_grid_layout.addWidget(_run_tx_calibration_push_button, 12, 0, 1, 1)
-        for r in range(12, 13):
-            self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(0, 1):
-            self.top_grid_layout.setColumnStretch(c, 1)
         self.analog_sig_source_x_0 = analog.sig_source_c(sample_rate, analog.GR_COS_WAVE, tone_freq, 1, 0, 0)
-        self.analog_sig_source_x_0.set_min_output_buffer(min_output_buffer)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_sig_source_x_0, 0), (self.sidekiq_sidekiq_tx_0, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.soapy_custom_sink_0, 0))
 
 
     def closeEvent(self, event):
@@ -157,7 +158,6 @@ class sink_test(gr.top_block, Qt.QWidget):
         self.sample_rate = sample_rate
         self.set_bandwidth(self.sample_rate * 0.8)
         self.analog_sig_source_x_0.set_sampling_freq(self.sample_rate)
-        self.sidekiq_sidekiq_tx_0.set_tx_sample_rate(self.sample_rate)
 
     def get_tone_freq(self):
         return self.tone_freq
@@ -171,43 +171,37 @@ class sink_test(gr.top_block, Qt.QWidget):
 
     def set_run_tx_calibration(self, run_tx_calibration):
         self.run_tx_calibration = run_tx_calibration
-        self.sidekiq_sidekiq_tx_0.run_tx_cal(self.run_tx_calibration)
 
-    def get_min_output_buffer(self):
-        return self.min_output_buffer
+    def get_min_buffer(self):
+        return self.min_buffer
 
-    def set_min_output_buffer(self, min_output_buffer):
-        self.min_output_buffer = min_output_buffer
+    def set_min_buffer(self, min_buffer):
+        self.min_buffer = min_buffer
 
     def get_frequency(self):
         return self.frequency
 
     def set_frequency(self, frequency):
         self.frequency = frequency
-        self.sidekiq_sidekiq_tx_0.set_tx_frequency(self.frequency)
+        self.soapy_custom_sink_0.set_frequency(0, self.frequency)
 
     def get_bandwidth(self):
         return self.bandwidth
 
     def set_bandwidth(self, bandwidth):
         self.bandwidth = bandwidth
-        self.sidekiq_sidekiq_tx_0.set_tx_bandwidth(self.bandwidth)
 
     def get_attenuation(self):
         return self.attenuation
 
     def set_attenuation(self, attenuation):
         self.attenuation = attenuation
-        self.sidekiq_sidekiq_tx_0.set_tx_attenuation(self.attenuation)
 
 
 
 
 def main(top_block_cls=sink_test, options=None):
 
-    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-        style = gr.prefs().get_string('qtgui', 'style', 'raster')
-        Qt.QApplication.setGraphicsSystem(style)
     qapp = Qt.QApplication(sys.argv)
 
     tb = top_block_cls()
--- examples/source_test.py
+++ examples/source_test.py
@@ -6,38 +6,25 @@
 #
 # GNU Radio Python Flow Graph
 # Title: source_test
-# GNU Radio version: v3.11.0.0git-375-ge2af6089
-
-from packaging.version import Version as StrictVersion
-
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print("Warning: failed to XInitThreads()")
+# GNU Radio version: v3.11.0.0git-605-g9b22fd38
 
 from PyQt5 import Qt
 from gnuradio import qtgui
-from gnuradio.filter import firdes
-import sip
 from gnuradio import gr
+from gnuradio.filter import firdes
 from gnuradio.fft import window
 import sys
 import signal
+from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
-from gnuradio import sidekiq
+from gnuradio import soapy
 from gnuradio.qtgui import Range, RangeWidget
 from PyQt5 import QtCore
+import sip
 
 
-
-from gnuradio import qtgui
 
 class source_test(gr.top_block, Qt.QWidget):
 
@@ -48,8 +35,8 @@ class source_test(gr.top_block, Qt.QWidget):
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-            pass
+        except BaseException as exc:
+            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
         self.top_scroll_layout = Qt.QVBoxLayout()
         self.setLayout(self.top_scroll_layout)
         self.top_scroll = Qt.QScrollArea()
@@ -65,17 +52,16 @@ class source_test(gr.top_block, Qt.QWidget):
         self.settings = Qt.QSettings("GNU Radio", "source_test")
 
         try:
-            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-                self.restoreGeometry(self.settings.value("geometry").toByteArray())
-            else:
-                self.restoreGeometry(self.settings.value("geometry"))
-        except:
-            pass
+            geometry = self.settings.value("geometry")
+            if geometry:
+                self.restoreGeometry(geometry)
+        except BaseException as exc:
+            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
 
         ##################################################
         # Variables
         ##################################################
-        self.sample_rate = sample_rate = 20e6
+        self.sample_rate = sample_rate = 50e6
         self.samp_rate = samp_rate = 32000
         self.run_rx_calibration = run_rx_calibration = 0
         self.gain_index = gain_index = 10
@@ -86,20 +72,30 @@ class source_test(gr.top_block, Qt.QWidget):
         # Blocks
         ##################################################
 
-        self._sample_rate_range = Range(1e6, 250e6, 1e6, 20e6, 200)
+        self._sample_rate_range = Range(1e6, 250e6, 1e6, 50e6, 200)
         self._sample_rate_win = RangeWidget(self._sample_rate_range, self.set_sample_rate, "'sample_rate'", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._sample_rate_win)
-        self._gain_index_range = Range(0, 255, 1, 10, 200)
-        self._gain_index_win = RangeWidget(self._gain_index_range, self.set_gain_index, "Gain Index", "counter_slider", int, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._gain_index_win)
         self._frequency_range = Range(250e6, 6000e6, 1e6, 1000e6, 200)
         self._frequency_win = RangeWidget(self._frequency_range, self.set_frequency, "'frequency'", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._frequency_win)
-        self._bandwidth_range = Range(1e6, 250e6, 1e6, sample_rate * .8, 200)
-        self._bandwidth_win = RangeWidget(self._bandwidth_range, self.set_bandwidth, "'bandwidth'", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._bandwidth_win)
-        self.sidekiq_sidekiq_rx_0 = sidekiq.sidekiq_rx(1, 0, 100, sample_rate, bandwidth, frequency, 1, gain_index, 0, 0, 0, 0, 2)
-        self.sidekiq_sidekiq_rx_0.set_max_output_buffer(32000)
+        self.soapy_custom_source_0 = None
+        dev = 'driver=' + 'sidekiq'
+        stream_args = ''
+        tune_args = ['']
+        settings = ['']
+        self.soapy_custom_source_0 = soapy.source(dev, "fc32",
+                                  1, 'card=2',
+                                  stream_args, tune_args, settings)
+        self.soapy_custom_source_0.set_sample_rate(0, sample_rate)
+        self.soapy_custom_source_0.set_bandwidth(0, 0)
+        self.soapy_custom_source_0.set_antenna(0, 'RX')
+        self.soapy_custom_source_0.set_frequency(0, frequency)
+        self.soapy_custom_source_0.set_frequency_correction(0, 0)
+        self.soapy_custom_source_0.set_gain_mode(0, True)
+        self.soapy_custom_source_0.set_gain(0, 10)
+        self.soapy_custom_source_0.set_dc_offset_mode(0, True)
+        self.soapy_custom_source_0.set_dc_offset(0, 0)
+        self.soapy_custom_source_0.set_iq_balance(0, 0)
         _run_rx_calibration_push_button = Qt.QPushButton('Run RX Calibration')
         _run_rx_calibration_push_button = Qt.QPushButton('Run RX Calibration')
         self._run_rx_calibration_choices = {'Pressed': 1, 'Released': 0}
@@ -199,13 +195,19 @@ class source_test(gr.top_block, Qt.QWidget):
 
         self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
+        self._gain_index_range = Range(0, 255, 1, 10, 200)
+        self._gain_index_win = RangeWidget(self._gain_index_range, self.set_gain_index, "Gain Index", "counter_slider", int, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._gain_index_win)
+        self._bandwidth_range = Range(1e6, 250e6, 1e6, sample_rate * .8, 200)
+        self._bandwidth_win = RangeWidget(self._bandwidth_range, self.set_bandwidth, "'bandwidth'", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._bandwidth_win)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.sidekiq_sidekiq_rx_0, 0), (self.qtgui_freq_sink_x_0, 0))
-        self.connect((self.sidekiq_sidekiq_rx_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.soapy_custom_source_0, 0), (self.qtgui_freq_sink_x_0, 0))
+        self.connect((self.soapy_custom_source_0, 0), (self.qtgui_time_sink_x_0, 0))
 
 
     def closeEvent(self, event):
@@ -223,7 +225,6 @@ class source_test(gr.top_block, Qt.QWidget):
         self.sample_rate = sample_rate
         self.set_bandwidth(self.sample_rate * .8)
         self.qtgui_freq_sink_x_0.set_frequency_range(self.frequency, self.sample_rate)
-        self.sidekiq_sidekiq_rx_0.set_rx_sample_rate(self.sample_rate)
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -236,14 +237,12 @@ class source_test(gr.top_block, Qt.QWidget):
 
     def set_run_rx_calibration(self, run_rx_calibration):
         self.run_rx_calibration = run_rx_calibration
-        self.sidekiq_sidekiq_rx_0.run_rx_cal(self.run_rx_calibration)
 
     def get_gain_index(self):
         return self.gain_index
 
     def set_gain_index(self, gain_index):
         self.gain_index = gain_index
-        self.sidekiq_sidekiq_rx_0.set_rx_gain_index(self.gain_index)
 
     def get_frequency(self):
         return self.frequency
@@ -251,23 +250,19 @@ class source_test(gr.top_block, Qt.QWidget):
     def set_frequency(self, frequency):
         self.frequency = frequency
         self.qtgui_freq_sink_x_0.set_frequency_range(self.frequency, self.sample_rate)
-        self.sidekiq_sidekiq_rx_0.set_rx_frequency(self.frequency)
+        self.soapy_custom_source_0.set_frequency(0, self.frequency)
 
     def get_bandwidth(self):
         return self.bandwidth
 
     def set_bandwidth(self, bandwidth):
         self.bandwidth = bandwidth
-        self.sidekiq_sidekiq_rx_0.set_rx_bandwidth(self.bandwidth)
 
 
 
 
 def main(top_block_cls=source_test, options=None):
 
-    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-        style = gr.prefs().get_string('qtgui', 'style', 'raster')
-        Qt.QApplication.setGraphicsSystem(style)
     qapp = Qt.QApplication(sys.argv)
 
     tb = top_block_cls()
